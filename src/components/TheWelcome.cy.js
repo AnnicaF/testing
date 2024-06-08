851d444808019d96e6571993b7ff6150d77c97b1
Annica <annif011098@gmail.com>
2024-06-08 13:47:51 +0200

    delete cy files

--- src/components/TheWelcome.cy.js
+++
@@ -1,8 +0,0 @@
-import TheWelcome from './TheWelcome.vue'
-
-describe('<TheWelcome />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(TheWelcome)
-  })
-})
