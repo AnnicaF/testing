60e88a26385b7ac17844f06b8982b3c4973f9a9c
Annica <annif011098@gmail.com>
2024-06-09 14:36:49 +0200

    add one unit test

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,20 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from "vitest";
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import { mount } from "@vue/test-utils";
+import HelloWorld from "../HelloWorld.vue";
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+describe("HelloWorld", () => {
+  it("renders properly", () => {
+    const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } });
+    expect(wrapper.text()).toContain("Hello Vitest");
+  });
+});
+
+describe("HelloWorld", () => {
+  it("tests inputfield", () => {
+    const wrapper = mount(HelloWorld);
+    const input = wrapper.find("input");
+    input.setValue("Hello World Input");
+    expect(input.element.value).toBe("Hello World Input");
+  });
+});
